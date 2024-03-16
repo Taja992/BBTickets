dcf56a753a9f56b2cb7e8a5b51ce4c1e9b5d14c8
Taja992 <Tharaxui@gmail.com>
2024-03-16 12:52:58 +0100

    Added a way to add new events Added personalized Exceptions Added css and linked it to fxml

--- BBTickets/src/main/BLL/BLLEvent.java
+++ BBTickets/src/main/BLL/BLLEvent.java
@@ -1,4 +1,14 @@
 package BLL;
 
+import BE.Event;
+import DAL.EventDAO;
+import Exceptions.BBExceptions;
+
 public class BLLEvent {
+
+    EventDAO DAO = new EventDAO();
+
+    public void newEvent(Event event) throws BBExceptions {
+        DAO.newEvent(event);
+    }
 }
--- BBTickets/src/main/GUI/controller/AppController.java
+++ BBTickets/src/main/GUI/controller/AppController.java
@@ -1,4 +1,50 @@
 package GUI.controller;
 
+import BE.Event;
+import BLL.BLLEvent;
+import Exceptions.BBExceptions;
+import com.jfoenix.controls.JFXTimePicker;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 public class AppController {
+    public TextField eventTypeField;
+    public TextField eventLocationField;
+    public DatePicker eventStartDatePicker;
+    public Spinner<Integer> eventHourSpinner;
+    public Spinner<Integer> eventMinuteSpinner;
+
+
+    @FXML
+    private Button addEvent;
+    private final BLLEvent bllEvent;
+
+    public AppController(){
+        bllEvent = new BLLEvent();
+    }
+
+    public void initialize() {
+        SpinnerValueFactory<Integer> valueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 59, 0, 15);
+        eventMinuteSpinner.setValueFactory(valueFactory);
+    }
+
+
+    public void addEvent(ActionEvent actionEvent) {
+        String eventType = eventTypeField.getText();
+        String eventLocation = eventLocationField.getText();
+        int hour = eventHourSpinner.getValue();
+        int minute = eventMinuteSpinner.getValue();
+        LocalDateTime eventStartTime = LocalDateTime.of(eventStartDatePicker.getValue(), LocalTime.of(hour, minute));
+        Event event = new Event(eventType, eventLocation, eventStartTime);
+
+        try {
+            bllEvent.newEvent(event);
+        } catch (BBExceptions e) {
+            e.printStackTrace();
+        }
+    }
 }
