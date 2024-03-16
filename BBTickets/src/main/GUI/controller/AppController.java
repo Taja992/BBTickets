d3d8f89873714077d7da5f58492d12b4144561d1
Taja992 <Tharaxui@gmail.com>
2024-03-16 17:43:22 +0100

    Set up Add Event button to open a new window to create event

--- BBTickets/src/main/GUI/controller/AppController.java
+++ BBTickets/src/main/GUI/controller/AppController.java
@@ -5,19 +5,34 @@ import BLL.BLLEvent;
 import Exceptions.BBExceptions;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 public class AppController {
-    public TextField eventTypeField;
-    public TextField eventLocationField;
-    public DatePicker eventStartDatePicker;
-    public Spinner<Integer> eventHourSpinner;
-    public Spinner<Integer> eventMinuteSpinner;
 
-
+    @FXML
+    private TextField eventTypeField;
+    @FXML
+    private TextField eventLocationField;
+    @FXML
+    private DatePicker eventStartDatePicker;
+    @FXML
+    private Spinner<Integer> eventHourSpinner;
+    @FXML
+    private Spinner<Integer> eventMinuteSpinner;
+    @FXML
+    private DatePicker eventEndDatePicker;
+    @FXML
+    private TextField eventNotesField;
+    @FXML
+    private TextField locationGuidanceField;
     @FXML
     private Button addEvent;
     private final BLLEvent bllEvent;
@@ -27,23 +42,19 @@ public class AppController {
     }
 
     public void initialize() {
-        SpinnerValueFactory<Integer> valueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 59, 0, 15);
-        eventMinuteSpinner.setValueFactory(valueFactory);
     }
 
 
     public void addEvent(ActionEvent actionEvent) {
-        String eventType = eventTypeField.getText();
-        String eventLocation = eventLocationField.getText();
-        int hour = eventHourSpinner.getValue();
-        int minute = eventMinuteSpinner.getValue();
-        LocalDateTime eventStartTime = LocalDateTime.of(eventStartDatePicker.getValue(), LocalTime.of(hour, minute));
-        Event event = new Event(eventType, eventLocation, eventStartTime);
-
-        try {
-            bllEvent.newEvent(event);
-        } catch (BBExceptions e) {
-            e.printStackTrace();
+            try {
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/path/to/CreateEvent.fxml"));
+                Parent root = fxmlLoader.load();
+                Stage stage = new Stage();
+                stage.setScene(new Scene(root));
+                stage.show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-    }
+
 }
