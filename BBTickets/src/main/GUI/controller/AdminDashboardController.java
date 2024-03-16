7a7492a0fc64c483c81877e67ebf3c3d1e4ca97c
thomasbrowne0 <reapple119@gmail.com>
2024-03-16 23:43:20 +0100

    visual bug fixed (white corners)

--- BBTickets/src/main/GUI/controller/AdminDashboardController.java
+++ BBTickets/src/main/GUI/controller/AdminDashboardController.java
@@ -1,18 +1,16 @@
 package GUI.controller;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
-import java.awt.*;
 import java.io.IOException;
 
 public class AdminDashboardController {
-
-
     public Button logoutBtn;
 
     public void initialize() {
@@ -21,13 +19,22 @@ public class AdminDashboardController {
                 // Load login.fxml
                 Parent root = FXMLLoader.load(getClass().getResource("/GUI/view/login.fxml"));
 
-                // Get the current stage
-                Stage stage = (Stage) logoutBtn.getScene().getWindow();
+                // Create a new stage for the login screen
+                Stage loginStage = new Stage();
+                loginStage.initStyle(StageStyle.TRANSPARENT);
 
                 // Create a new scene with the loaded parent and set it on the stage
                 Scene scene = new Scene(root);
-                stage.setScene(scene);
-                stage.show();
+                scene.setFill(Color.TRANSPARENT);
+                loginStage.setTitle("Ticket Interface");
+                loginStage.setScene(scene);
+
+                // Get the current stage and close it
+                Stage currentStage = (Stage) logoutBtn.getScene().getWindow();
+                currentStage.close();
+
+                // Show the login stage
+                loginStage.show();
             } catch (IOException e) {
                 e.printStackTrace();
             }
