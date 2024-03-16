94dc939f58d68115f8908660793c56d91a8ffe30
thomasbrowne0 <reapple119@gmail.com>
2024-03-16 23:13:07 +0100

    logging in as admin opens dashboard

--- BBTickets/src/main/GUI/controller/AdminDashboardController.java
+++ BBTickets/src/main/GUI/controller/AdminDashboardController.java
@@ -1,4 +1,36 @@
 package GUI.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.awt.*;
+import java.io.IOException;
+
 public class AdminDashboardController {
+
+
+    public Button logoutBtn;
+
+    public void initialize() {
+        logoutBtn.setOnAction(event -> {
+            try {
+                // Load login.fxml
+                Parent root = FXMLLoader.load(getClass().getResource("/GUI/view/login.fxml"));
+
+                // Get the current stage
+                Stage stage = (Stage) logoutBtn.getScene().getWindow();
+
+                // Create a new scene with the loaded parent and set it on the stage
+                Scene scene = new Scene(root);
+                stage.setScene(scene);
+                stage.show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
 }
--- BBTickets/src/main/GUI/controller/LoginController.java
+++ BBTickets/src/main/GUI/controller/LoginController.java
@@ -3,8 +3,12 @@ package GUI.controller;
 import BE.User;
 import BLL.BLLUser;
 import DAL.UserDAO;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class LoginController {
     public Button loginBtn;
@@ -26,13 +30,21 @@ public class LoginController {
             try {
                 User user = bllUser.getUser(username, password);
                 if (user != null) {
+                    Stage stage = (Stage) loginBtn.getScene().getWindow();
+                    Parent root = null;
                     if (user.getUser_type() == 1) {
                         // Open admin dashboard
                         System.out.println("Admin logged in");
-                    } else {
+                        root = FXMLLoader.load(getClass().getResource("/GUI/view/adminDashboard.fxml"));
+
+                    } else if ((user.getUser_type() == 0)) {
                         // Open event coordinator dashboard
                         System.out.println("Event coordinator logged in");
+                        root = FXMLLoader.load(getClass().getResource("/GUI/view/ecDashboard.fxml"));
                     }
+                    Scene scene = new Scene(root);
+                    stage.setScene(scene);
+                    stage.show();
                 } else {
                     System.out.println("Invalid username or password");
                 }
