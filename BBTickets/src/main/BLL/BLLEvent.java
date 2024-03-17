4933dec075b4d92cefb4878f879eb65d33cc02ef
Taja992 <Tharaxui@gmail.com>
2024-03-17 13:08:30 +0100

    -Got the Dashboards to properly show their stages -Set up the Event Table on Admin Dashboard to show events

--- BBTickets/src/main/BLL/BLLEvent.java
+++ BBTickets/src/main/BLL/BLLEvent.java
@@ -4,11 +4,17 @@ import BE.Event;
 import DAL.EventDAO;
 import Exceptions.BBExceptions;
 
+import java.util.List;
+
 public class BLLEvent {
 
     EventDAO DAO = new EventDAO();
 
     public void newEvent(Event event) throws BBExceptions {
         DAO.newEvent(event);
+    }
+
+    public List<Event> getAllEvents() throws BBExceptions {
+        return DAO.getAllEvents();
     }
 }
