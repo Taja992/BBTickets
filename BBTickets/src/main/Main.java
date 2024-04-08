279c0dc52a8d7c940739929cf3cda8ff3ff204f1
Taja992 <Tharaxui@gmail.com>
2024-04-08 11:39:25 +0200

    styling

--- BBTickets/src/main/Main.java
+++ BBTickets/src/main/Main.java
@@ -1,19 +1,13 @@
-import DAL.ConnectionManager;
-import GUI.util.FontLoadUtil;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
-
-import java.sql.Connection;
-import java.sql.SQLException;
 
 public class Main extends Application {
 
@@ -25,7 +19,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FontLoadUtil.loadFont("https://fonts.gstatic.com/s/roboto/v27/KFOmCnqEu92Fr1Mu4mxP.ttf", 20);
+        Font.loadFont(getClass().getResourceAsStream("/fonts/PTSerif-Regular.ttf"), 20);
+        Font.loadFont(getClass().getResourceAsStream("/fonts/Lato-Regular.ttf"), 20);
+        Font.loadFont(getClass().getResourceAsStream("/fonts/Roboto-Regular.ttf"), 20);
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("GUI/view/login.fxml"));
         Parent root = fxmlLoader.load();
         Scene scene = new Scene(root);
