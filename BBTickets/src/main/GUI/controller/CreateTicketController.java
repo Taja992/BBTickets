dda19336248b77fd3db11e5c7bffe6523f3c1e4b
thomasbrowne0 <reapple119@gmail.com>
2024-04-10 23:30:04 +0200

    styled coupon window with css

--- BBTickets/src/main/GUI/controller/CreateTicketController.java
+++ BBTickets/src/main/GUI/controller/CreateTicketController.java
@@ -5,6 +5,7 @@ import BE.Customer;
 import BE.Event;
 import BE.TicketType;
 import Exceptions.BBExceptions;
+import GUI.model.CouponModel;
 import GUI.model.CustomerModel;
 import GUI.model.TicketModel;
 import javafx.event.ActionEvent;
@@ -29,7 +30,7 @@ import java.util.ResourceBundle;
 public class CreateTicketController implements Initializable {
 
 
-
+    public ChoiceBox couponChkBox;
     @FXML
     private ListView<Customer> customerLv;
     @FXML
@@ -51,9 +52,11 @@ public class CreateTicketController implements Initializable {
     private CustomerModel custModel = new CustomerModel();
     private List<Customer> allCustomers = new ArrayList<>();
     private Event selectedEvent;
+    private CouponModel couponModel = new CouponModel();
 
     private List<TicketType> types = new ArrayList<>();
     private List<String> typesForBox = new ArrayList<>();
+    List<String> couponNotes = couponModel.getAllCouponNotes();
 
     private String[] ticketTypes = {"Standard", "VIP"};
     private String uuid = "";
@@ -73,6 +76,8 @@ public class CreateTicketController implements Initializable {
 
         typeChcBox.getItems().addAll(typesForBox);
         typeChcBox.setValue("Standard");
+        couponChkBox.getItems().addAll(couponNotes);
+        couponChkBox.setValue("No Coupon");
     }
 
     public void setCreateTicketBtn(Button createTicketBtn) {
